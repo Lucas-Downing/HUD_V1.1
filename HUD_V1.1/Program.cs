29af54d15b6e8e90b50b6a313b8eaae202233a0a
Lucas-Downing <90638796+Lucas-Downing@users.noreply.github.com>
2021-09-27 14:11:12 -0300

    Title

--- HUD_V1.1/Program.cs
+++ HUD_V1.1/Program.cs
@@ -8,8 +8,46 @@ namespace HUD_V1._1
 {
     class Program
     {
+
+        static int health;
+        static int score;
+        static int xp;
+        
+
         static void Main(string[] args)
         {
+            Title();
+            Armour(25);
+            HUD();
+        }
+
+
+        static void Damage()
+        {
+
+        }
+        static void HUD()
+        {
+            Console.WriteLine("Health = " + health);
+            Console.WriteLine("Armour = " + armour);
+            Console.ReadKey(true);
+        }
+
+        static void Armour(int armour)
+        {
+            
+        }
+
+        static void Title()
+        {
+            Console.WriteLine("+++++++++++++++++++++++++");
+            Console.WriteLine();
+            Console.WriteLine("Welcome to working_title!");
+            Console.WriteLine();
+            Console.WriteLine("buy a better game....");
+            Console.WriteLine("+++++++++++++++++++++++++");
+            Console.WriteLine();
+            Console.ReadKey(true);
         }
     }
 }
